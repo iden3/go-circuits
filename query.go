14905ef13acde2a18f51692b4fc730148e17111f
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2021-12-16 18:21:14 +0200

    verification key updates and refactor

---
+++ query.go
@@ -0,0 +1,19 @@
+package circuits
+
+import "math/big"
+
+// Query represents basic request to claim slot verification
+type Query struct {
+	SlotIndex int
+	Value     *big.Int
+	Operator  int
+}
+
+// QueryOperators represents operators for atomic circuits
+var QueryOperators = map[string]int{
+	"$eq": 0,
+	"$lt": 1,
+	"$gt": 2,
+	"$ni": 3,
+	"$in": 4,
+}
