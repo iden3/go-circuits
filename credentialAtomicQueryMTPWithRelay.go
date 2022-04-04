648c3751af7b29ff2e0743aa62f586c409a8462b
Andriian Chestnykh <andriian@snowball.money>
2022-04-04 18:58:33 +0300

    Rename credentialAtomicQueryMTPWithRelay inputs

--- credentialAtomicQueryMTPWithRelay.go
+++ credentialAtomicQueryMTPWithRelay.go
@@ -87,47 +87,47 @@ func (c *AtomicQueryMTPWithRelay) PrepareInputs(in TypedInputs) (map[string]inte
 func (c *AtomicQueryMTPWithRelay) prepareRegularClaimInputs(claim Claim, rs RevocationStatus) (map[string]interface{}, error) {
 
 	inputs := map[string]interface{}{
-		"claim": bigIntArrayToStringArray(claim.Slots),
-		"claimIssuanceMtp": bigIntArrayToStringArray(
+		"issuerClaim": bigIntArrayToStringArray(claim.Slots),
+		"issuerClaimMtp": bigIntArrayToStringArray(
 			PrepareSiblings(claim.Proof.Siblings, LevelsAtomicQueryMTPWithRelayCircuit)),
-		"claimIssuanceClaimsTreeRoot": claim.TreeState.
+		"issuerClaimClaimsTreeRoot": claim.TreeState.
 			ClaimsRootStr(),
-		"claimIssuanceRevTreeRoot": claim.TreeState.
+		"issuerClaimRevTreeRoot": claim.TreeState.
 			RevocationRootStr(),
-		"claimIssuanceRootsTreeRoot": claim.TreeState.
+		"issuerClaimRootsTreeRoot": claim.TreeState.
 			RootOfRootsRootStr(),
-		"claimIssuanceIdenState": claim.TreeState.StateStr(),
-		"issuerID":               claim.IssuerID.BigInt().String(),
+		"issuerClaimIdenState": claim.TreeState.StateStr(),
+		"issuerID":             claim.IssuerID.BigInt().String(),
 	}
 
 	// revocation
-	inputs["claimNonRevIssuerState"] = rs.TreeState.StateStr()
-	inputs["claimNonRevIssuerRootsTreeRoot"] = rs.TreeState.
+	inputs["issuerClaimNonRevState"] = rs.TreeState.StateStr()
+	inputs["issuerClaimNonRevRootsTreeRoot"] = rs.TreeState.
 		RootOfRootsRootStr()
-	inputs["claimNonRevIssuerRevTreeRoot"] = rs.TreeState.
+	inputs["issuerClaimNonRevRevTreeRoot"] = rs.TreeState.
 		RevocationRootStr()
-	inputs["claimNonRevIssuerClaimsTreeRoot"] = rs.TreeState.
+	inputs["issuerClaimNonRevClaimsTreeRoot"] = rs.TreeState.
 		ClaimsRootStr()
 
 	// claim non revocation
 
-	inputs["claimNonRevMtp"] = bigIntArrayToStringArray(PrepareSiblings(rs.Proof.Siblings, LevelsAtomicQueryMTPWithRelayCircuit))
+	inputs["issuerClaimNonRevMtp"] = bigIntArrayToStringArray(PrepareSiblings(rs.Proof.Siblings, LevelsAtomicQueryMTPWithRelayCircuit))
 
 	if rs.Proof.NodeAux == nil {
-		inputs["claimNonRevMtpAuxHi"] = merkletree.HashZero.BigInt().String()
-		inputs["claimNonRevMtpAuxHv"] = merkletree.HashZero.BigInt().String()
-		inputs["claimNonRevMtpNoAux"] = new(big.Int).SetInt64(1).String() // (yes it's isOld = 1)
+		inputs["issuerClaimNonRevMtpAuxHi"] = merkletree.HashZero.BigInt().String()
+		inputs["issuerClaimNonRevMtpAuxHv"] = merkletree.HashZero.BigInt().String()
+		inputs["issuerClaimNonRevMtpNoAux"] = new(big.Int).SetInt64(1).String() // (yes it's isOld = 1)
 	} else {
-		inputs["claimNonRevMtpNoAux"] = new(big.Int).SetInt64(0).String() // (no it's isOld = 0)
+		inputs["issuerClaimNonRevMtpNoAux"] = new(big.Int).SetInt64(0).String() // (no it's isOld = 0)
 		if rs.Proof.NodeAux.HIndex == nil {
-			inputs["claimNonRevMtpAuxHi"] = merkletree.HashZero.BigInt().String()
+			inputs["issuerClaimNonRevMtpAuxHi"] = merkletree.HashZero.BigInt().String()
 		} else {
-			inputs["claimNonRevMtpAuxHi"] = rs.Proof.NodeAux.HIndex.BigInt().String()
+			inputs["issuerClaimNonRevMtpAuxHi"] = rs.Proof.NodeAux.HIndex.BigInt().String()
 		}
 		if rs.Proof.NodeAux.HValue == nil {
-			inputs["claimNonRevMtpAuxHv"] = merkletree.HashZero.BigInt().String()
+			inputs["issuerClaimNonRevMtpAuxHv"] = merkletree.HashZero.BigInt().String()
 		} else {
-			inputs["claimNonRevMtpAuxHv"] = rs.Proof.NodeAux.HValue.BigInt().String()
+			inputs["issuerClaimNonRevMtpAuxHv"] = rs.Proof.NodeAux.HValue.BigInt().String()
 		}
 	}
 
@@ -148,8 +148,8 @@ func (c *AtomicQueryMTPWithRelay) prepareAuthClaimInputs(in *AtomicQueryMTPWithR
 	inputs["userID"] = in.ID.BigInt().String()
 	inputs["challenge"] = in.Challenge.String()
 
-	inputs["authClaim"] = bigIntArrayToStringArray(in.AuthClaim.Slots)
-	inputs["authClaimMtp"] = bigIntArrayToStringArray(
+	inputs["userAuthClaim"] = bigIntArrayToStringArray(in.AuthClaim.Slots)
+	inputs["userAuthClaimMtp"] = bigIntArrayToStringArray(
 		PrepareSiblings(in.AuthClaim.Proof.Siblings, LevelsAtomicQueryMTPWithRelayCircuit))
 
 	// Note: we don't setup inputs user state, e.g. ["hoIdenState"] = in.CurrentStateTree.StateStr() here
@@ -158,23 +158,23 @@ func (c *AtomicQueryMTPWithRelay) prepareAuthClaimInputs(in *AtomicQueryMTPWithR
 	inputs["userRevTreeRoot"] = in.CurrentStateTree.RevocationRootStr()
 	inputs["userRootsTreeRoot"] = in.CurrentStateTree.RootOfRootsRootStr()
 
-	inputs["authClaimNonRevMtp"] = bigIntArrayToStringArray(PrepareSiblings(in.AuthClaimRevStatus.Proof.Siblings, LevelsAtomicQueryMTPWithRelayCircuit))
+	inputs["userAuthClaimNonRevMtp"] = bigIntArrayToStringArray(PrepareSiblings(in.AuthClaimRevStatus.Proof.Siblings, LevelsAtomicQueryMTPWithRelayCircuit))
 
 	if in.AuthClaimRevStatus.Proof.NodeAux == nil {
-		inputs["authClaimNonRevMtpAuxHv"] = merkletree.HashZero.BigInt().String()
-		inputs["authClaimNonRevMtpAuxHi"] = merkletree.HashZero.BigInt().String()
-		inputs["authClaimNonRevMtpNoAux"] = new(big.Int).SetInt64(1).String() // (yes it's isOld = 1)
+		inputs["userAuthClaimNonRevMtpAuxHv"] = merkletree.HashZero.BigInt().String()
+		inputs["userAuthClaimNonRevMtpAuxHi"] = merkletree.HashZero.BigInt().String()
+		inputs["userAuthClaimNonRevMtpNoAux"] = new(big.Int).SetInt64(1).String() // (yes it's isOld = 1)
 	} else {
-		inputs["authClaimNonRevMtpNoAux"] = new(big.Int).SetInt64(0).String() // (no it's isOld = 0)
+		inputs["userAuthClaimNonRevMtpNoAux"] = new(big.Int).SetInt64(0).String() // (no it's isOld = 0)
 		if in.AuthClaimRevStatus.Proof.NodeAux.HIndex == nil {
-			inputs["authClaimNonRevMtpAuxHi"] = merkletree.HashZero.BigInt().String()
+			inputs["userAuthClaimNonRevMtpAuxHi"] = merkletree.HashZero.BigInt().String()
 		} else {
-			inputs["authClaimNonRevMtpAuxHi"] = in.AuthClaimRevStatus.Proof.NodeAux.HIndex.BigInt().String()
+			inputs["userAuthClaimNonRevMtpAuxHi"] = in.AuthClaimRevStatus.Proof.NodeAux.HIndex.BigInt().String()
 		}
 		if in.AuthClaimRevStatus.Proof.NodeAux.HValue == nil {
-			inputs["authClaimNonRevMtpAuxHv"] = merkletree.HashZero.BigInt().String()
+			inputs["userAuthClaimNonRevMtpAuxHv"] = merkletree.HashZero.BigInt().String()
 		} else {
-			inputs["authClaimNonRevMtpAuxHv"] = in.AuthClaimRevStatus.Proof.NodeAux.HValue.BigInt().String()
+			inputs["userAuthClaimNonRevMtpAuxHv"] = in.AuthClaimRevStatus.Proof.NodeAux.HValue.BigInt().String()
 		}
 	}
 
--- credentialAtomicQueryMTPWithRelay_test.go
+++ credentialAtomicQueryMTPWithRelay_test.go
@@ -24,7 +24,7 @@ func TestAtomicQueryMTPWithRelay_PrepareInputs(t *testing.T) {
 	ctx := context.Background()
 
 	//User
-	userIdentity, uClaimsTree, _, _, err, userAuthClaim, userPrivateKey :=
+	userIdentity, uClaimsTree, _, _, err, userAuthCoreClaim, userPrivateKey :=
 		identity.Generate(ctx, userPrivKHex)
 	assert.Nil(t, err)
 
@@ -38,7 +38,7 @@ func TestAtomicQueryMTPWithRelay_PrepareInputs(t *testing.T) {
 	}
 	assert.Nil(t, err)
 
-	hIndexAuthEntryUser, _, err := claimsIndexValueHashes(*userAuthClaim)
+	hIndexAuthEntryUser, _, err := claimsIndexValueHashes(*userAuthCoreClaim)
 	require.NoError(t, err)
 
 	mtpProofUser, _, err := uClaimsTree.GenerateProof(ctx, hIndexAuthEntryUser,
@@ -101,7 +101,7 @@ func TestAtomicQueryMTPWithRelay_PrepareInputs(t *testing.T) {
 
 	copy(schemaHash[:], schemaBytes)
 
-	claim, err := core.NewClaim(
+	issuerCoreClaim, err := core.NewClaim(
 		schemaHash,
 		core.WithIndexID(*userIdentity),
 		core.WithIndexData(dataSlotA, core.ElemBytes{}),
@@ -110,7 +110,7 @@ func TestAtomicQueryMTPWithRelay_PrepareInputs(t *testing.T) {
 		core.WithRevocationNonce(uint64(nonce)))
 	assert.Nil(t, err)
 
-	hIndexClaimEntry, hValueClaimEntry, err := claimsIndexValueHashes(*claim)
+	hIndexClaimEntry, hValueClaimEntry, err := claimsIndexValueHashes(*issuerCoreClaim)
 	require.NoError(t, err)
 
 	err = iClaimsTree.Add(ctx, hIndexClaimEntry, hValueClaimEntry)
@@ -164,18 +164,18 @@ func TestAtomicQueryMTPWithRelay_PrepareInputs(t *testing.T) {
 		}
 	}
 
-	var authClaim Claim
+	var userAuthClaim Claim
 
 	inputsAuthClaim := Claim{
-		Schema:           authClaim.Schema,
-		Slots:            getSlots(userAuthClaim),
+		Schema:           userAuthClaim.Schema,
+		Slots:            getSlots(userAuthCoreClaim),
 		Proof:            mtpAuthUser,
 		TreeState:        userAuthTreeState,
 		CurrentTimeStamp: time.Unix(1642074362, 0).Unix(),
 	}
 
 	inputsUserStateInRelayClaim := Claim{
-		Schema:           authClaim.Schema,
+		Schema:           userAuthClaim.Schema,
 		Slots:            getSlots(claimUserStateInRelay),
 		Proof:            mtpUserStateInRelay,
 		TreeState:        relayTreeState,
@@ -183,8 +183,8 @@ func TestAtomicQueryMTPWithRelay_PrepareInputs(t *testing.T) {
 	}
 
 	inputsUserClaim := Claim{
-		Schema:           claim.GetSchemaHash(),
-		Slots:            getSlots(claim),
+		Schema:           issuerCoreClaim.GetSchemaHash(),
+		Slots:            getSlots(issuerCoreClaim),
 		Proof:            mtpClaimProof,
 		TreeState:        issuerStateAfterClaimAdd,
 		CurrentTimeStamp: time.Unix(1642074362, 0).Unix(),
@@ -235,7 +235,7 @@ func TestAtomicQueryMTPWithRelay_PrepareInputs(t *testing.T) {
 	bytesInputs, err := json.Marshal(inputs)
 	assert.Nil(t, err)
 
-	expectedJSONInputs := `{"authClaim":["269270088098491255471307608775043319525","0","17640206035128972995519606214765283372613874593503528180869261482403155458945","20634138280259599560273310290025659992320584624461316485434108770067472477956","15930428023331155902","0","0","0"],"authClaimMtp":["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"authClaimNonRevMtp":["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"authClaimNonRevMtpAuxHi":"0","authClaimNonRevMtpAuxHv":"0","authClaimNonRevMtpNoAux":"1","challenge":"1","challengeSignatureR8x":"8553678144208642175027223770335048072652078621216414881653012537434846327449","challengeSignatureR8y":"5507837342589329113352496188906367161790372084365285966741761856353367255709","challengeSignatureS":"2093461910575977345603199789919760192811763972089699387324401771367839603655","claim":["3677203805624134172815825715044445108615","286312392162647260160287083374160163061246635086990474403590223113720496128","10","0","30803922965249841627828060161","0","0","0"],"claimIssuanceClaimsTreeRoot":"12781049434766209895790529815771921100011665835724745028505992240548230711728","claimIssuanceIdenState":"20606705619830543359176597576564222044873771515109680973150322899613614552596","claimIssuanceMtp":["0","3007906543589053223183609977424583669571967498470079791401931468580200755448","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"claimIssuanceRevTreeRoot":"0","claimIssuanceRootsTreeRoot":"0","claimNonRevIssuerClaimsTreeRoot":"12781049434766209895790529815771921100011665835724745028505992240548230711728","claimNonRevIssuerRevTreeRoot":"0","claimNonRevIssuerRootsTreeRoot":"0","claimNonRevIssuerState":"20606705619830543359176597576564222044873771515109680973150322899613614552596","claimNonRevMtp":["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"claimNonRevMtpAuxHi":"0","claimNonRevMtpAuxHv":"0","claimNonRevMtpNoAux":"1","claimSchema":"274380136414749538182079640726762994055","issuerID":"296941560404583387587196218166209608454370683337298127000644446413747191808","operator":0,"relayProofValidClaimsTreeRoot":"2854665891046135459434995383199781762190358117579623998115936884038963331048","relayProofValidRevTreeRoot":"0","relayProofValidRootsTreeRoot":"0","relayState":"16294564286985950894527527840426853346844847075954975086655280191624111272054","slotIndex":2,"timestamp":"1642074362","userClaimsTreeRoot":"8033159210005724351649063848617878571712113104821846241291681963936214187701","userID":"286312392162647260160287083374160163061246635086990474403590223113720496128","userRevTreeRoot":"0","userRootsTreeRoot":"0","userStateInRelayClaim":["981208330819247466821056791934709559638","286312392162647260160287083374160163061246635086990474403590223113720496128","0","0","0","0","5816868615164565912277677884704888703982258184820398645933682814085602171910","0"],"userStateInRelayClaimMtp":["0","1501244652861114532352800692615798696848833011443509616387313576023182892460","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"value":["10","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"]}
+	expectedJSONInputs := `{"userAuthClaim":["269270088098491255471307608775043319525","0","17640206035128972995519606214765283372613874593503528180869261482403155458945","20634138280259599560273310290025659992320584624461316485434108770067472477956","15930428023331155902","0","0","0"],"userAuthClaimMtp":["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"userAuthClaimNonRevMtp":["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"userAuthClaimNonRevMtpAuxHi":"0","userAuthClaimNonRevMtpAuxHv":"0","userAuthClaimNonRevMtpNoAux":"1","challenge":"1","challengeSignatureR8x":"8553678144208642175027223770335048072652078621216414881653012537434846327449","challengeSignatureR8y":"5507837342589329113352496188906367161790372084365285966741761856353367255709","challengeSignatureS":"2093461910575977345603199789919760192811763972089699387324401771367839603655","issuerClaim":["3677203805624134172815825715044445108615","286312392162647260160287083374160163061246635086990474403590223113720496128","10","0","30803922965249841627828060161","0","0","0"],"issuerClaimClaimsTreeRoot":"12781049434766209895790529815771921100011665835724745028505992240548230711728","issuerClaimIdenState":"20606705619830543359176597576564222044873771515109680973150322899613614552596","issuerClaimMtp":["0","3007906543589053223183609977424583669571967498470079791401931468580200755448","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"issuerClaimRevTreeRoot":"0","issuerClaimRootsTreeRoot":"0","issuerClaimNonRevClaimsTreeRoot":"12781049434766209895790529815771921100011665835724745028505992240548230711728","issuerClaimNonRevRevTreeRoot":"0","issuerClaimNonRevRootsTreeRoot":"0","issuerClaimNonRevState":"20606705619830543359176597576564222044873771515109680973150322899613614552596","issuerClaimNonRevMtp":["0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"issuerClaimNonRevMtpAuxHi":"0","issuerClaimNonRevMtpAuxHv":"0","issuerClaimNonRevMtpNoAux":"1","claimSchema":"274380136414749538182079640726762994055","issuerID":"296941560404583387587196218166209608454370683337298127000644446413747191808","operator":0,"relayProofValidClaimsTreeRoot":"2854665891046135459434995383199781762190358117579623998115936884038963331048","relayProofValidRevTreeRoot":"0","relayProofValidRootsTreeRoot":"0","relayState":"16294564286985950894527527840426853346844847075954975086655280191624111272054","slotIndex":2,"timestamp":"1642074362","userClaimsTreeRoot":"8033159210005724351649063848617878571712113104821846241291681963936214187701","userID":"286312392162647260160287083374160163061246635086990474403590223113720496128","userRevTreeRoot":"0","userRootsTreeRoot":"0","userStateInRelayClaim":["981208330819247466821056791934709559638","286312392162647260160287083374160163061246635086990474403590223113720496128","0","0","0","0","5816868615164565912277677884704888703982258184820398645933682814085602171910","0"],"userStateInRelayClaimMtp":["0","1501244652861114532352800692615798696848833011443509616387313576023182892460","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"],"value":["10","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"]}
 `
 
 	var actualInputs map[string]interface{}
@@ -262,7 +262,7 @@ func generateRelayWithIdenStateClaim(relayPrivKeyHex string,
 	schemaEncodedBytes, _ := hex.DecodeString("e22dd9c0f7aef15788c130d4d86c7156")
 	copy(schemaHash[:], schemaEncodedBytes)
 	valueSlotA, _ := core.NewElemBytesFromInt(identityState.BigInt())
-	claim, err := core.NewClaim(
+	issuerClaim, err := core.NewClaim(
 		schemaHash,
 		core.WithIndexID(*identifier),
 		core.WithValueData(valueSlotA, core.ElemBytes{}),
@@ -271,7 +271,7 @@ func generateRelayWithIdenStateClaim(relayPrivKeyHex string,
 		return nil, nil, nil, nil, nil, nil, err
 	}
 
-	proofIdenStateInRelay, err := addClaimToTree(relayClaimsTree, claim)
+	proofIdenStateInRelay, err := addClaimToTree(relayClaimsTree, issuerClaim)
 	if err != nil {
 		return nil, nil, nil, nil, nil, nil, err
 	}
@@ -281,13 +281,13 @@ func generateRelayWithIdenStateClaim(relayPrivKeyHex string,
 		return nil, nil, nil, nil, nil, nil, err
 	}
 
-	return claim, proofIdenStateInRelay, relayClaimsTree.Root(), &merkletree.HashZero, &merkletree.HashZero, relayState, nil
+	return issuerClaim, proofIdenStateInRelay, relayClaimsTree.Root(), &merkletree.HashZero, &merkletree.HashZero, relayState, nil
 }
 
 //nolint:unused
 func addClaimToTree(tree *merkletree.MerkleTree,
-	claim *core.Claim) (*merkletree.Proof, error) {
-	index, value, err := claimsIndexValueHashes(*claim)
+	issuerClaim *core.Claim) (*merkletree.Proof, error) {
+	index, value, err := claimsIndexValueHashes(*issuerClaim)
 	if err != nil {
 		return nil, err
 	}
