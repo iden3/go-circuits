61370a54df1c0c076f61e647773168be18a98390
Dmytro Sukhyi <dmytro.sukhiy@gmail.com>
2022-11-05 15:15:04 +0200

    Rework types, add comments

--- types.go
+++ types.go
@@ -16,38 +16,45 @@ type ClaimWithSigProof struct {
 type ClaimWithMTPProof struct {
 	IssuerID    *core.ID
 	Claim       *core.Claim
-	MTProof     MTProof
-	NonRevProof MTProof // Claim non revocation proof
+	IncProof    MTProof // proof of inclusion `Claim` to the issuer claims tree
+	NonRevProof MTProof // proof of non revocation of the `Claim` in the issuer revocation tree
 }
 
-type ClaimWithGlobalAuthProof struct {
-	Claim          *core.Claim       `json:"claim"`
-	NonRevProof    MTProof           `json:"nonRevProof"`
-	MTProof        MTProof           `json:"mtProof"`
-	SignatureProof BJJSignatureProof `json:"signatureProof"`
-	GlobalTree     GlobalTree        `json:"globalTree"`
-}
+//type ClaimWithGlobalAuthProof struct {
+//	Claim          *core.Claim       `json:"claim"`
+//	IncProof       MTProof           `json:"incProof"`
+//	NonRevProof    MTProof           `json:"nonRevProof"`
+//	SignatureProof BJJSignatureProof `json:"signatureProof"`
+//}
 
+// BJJSignatureProof is a proof of issuer AuthClaim signature over a claim
 type BJJSignatureProof struct {
-	Signature             *babyjub.Signature `json:"signature"`
-	IssuerAuthClaim       *core.Claim        `json:"issuerAuthClaim"`
-	IssuerAuthClaimMTP    MTProof            `json:"issuerAuthClaimMTP"`
-	IssuerAuthNonRevProof MTProof            `json:"issuerAuthNonRevProof"` // IssuerAuthClaim non revocation proof
+	// Signature Signing the claim with the private key of the issuer associated with the issuerAuthClaim
+	Signature       *babyjub.Signature `json:"signature"`
+	IssuerAuthClaim *core.Claim        `json:"issuerAuthClaim"` // issuer AuthClaim
+	// IssuerAuthIncProof proof of inclusion of issuer AuthClaim to
+	// the issuer claims tree
+	IssuerAuthIncProof MTProof `json:"issuerAuthIncProof"`
+	// IssuerAuthNonRevProof proof of non revocation of issuer
+	// AuthClaim in the issuer the latest state
+	IssuerAuthNonRevProof MTProof `json:"issuerAuthNonRevProof"`
 }
 
 type MTProof struct {
-	Proof     *merkletree.Proof `json:"proof"`
-	TreeState TreeState         `json:"treeState"`
+	Proof     *merkletree.Proof `json:"proof"`     // Proof of inclusion to the Merkle Tree
+	TreeState TreeState         `json:"treeState"` // Identity state
 }
 
+// TreeState represents the identity state
 type TreeState struct {
-	State          *merkletree.Hash `json:"state"`
-	ClaimsRoot     *merkletree.Hash `json:"claimsRoot"`
-	RevocationRoot *merkletree.Hash `json:"revocationRoot"`
-	RootOfRoots    *merkletree.Hash `json:"rootOfRoots"`
+	State          *merkletree.Hash `json:"state"`          // identity state
+	ClaimsRoot     *merkletree.Hash `json:"claimsRoot"`     // claims tree root
+	RevocationRoot *merkletree.Hash `json:"revocationRoot"` // revocation tree root
+	RootOfRoots    *merkletree.Hash `json:"rootOfRoots"`    // root of roots tree root
 }
 
+// GlobalTree represents the state of the global identities tree published on the blockchain
 type GlobalTree struct {
-	Root  *merkletree.Hash  `json:"root"`
-	Proof *merkletree.Proof `json:"proof"`
+	Root  *merkletree.Hash  `json:"root"`  // global identities tree root
+	Proof *merkletree.Proof `json:"proof"` // proof of inclusion or non inclusion of the identity to the global identities tree
 }
